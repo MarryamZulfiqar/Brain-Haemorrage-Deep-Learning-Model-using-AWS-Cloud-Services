96c8d739bf933fb4a46a1e4a6d7776c17da191ec
MaryamZulfiqar <13319389+MarryamZulfiqar@users.noreply.github.com>
2020-07-01 13:10:21 +0500

    Add files via upload

--- lambda_function.py
+++ lambda_function.py
@@ -1,36 +1,84 @@
-import json
-import urllib
-import urllib.parse
-from urllib.parse import urlparse
-import boto3
-from PIL import Image
-import PIL.Image
 import os
-import sys
-import uuid
+import json
+import numpy as np
+import urllib.request
+import boto3
+import psycopg2
+from datetime import datetime
+import sagemaker
+from sagemaker.predictor import RealTimePredictor
 
-print('Loading function')
 
-s3 = boto3.client('s3')
-
-
-def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
-
-    # Get the object from the event and show its content type
+def lambda_handler(event=None, context=None):
+  try:
+    # Downloading New Case
+    s3 = boto3.client('s3')
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        url = '{}/{}/{}'.format(s3.meta.endpoint_url, bucket, key)
-        print("URL: " + url)
-        print("CONTENT TYPE: " + response['ContentType'])
-    
-        tmpkey = key.replace('/', '')
-        download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
-        im = s3.download_file(bucket, key, download_path)
+    new_case_url = '{}/{}/{}'.format(s3.meta.endpoint_url, bucket, key)
+    # new_case_url = 'https://res.cloudinary.com/itu/image/upload/v1592184341/100055CT000270_wuhm1w.png'
+    file_name = new_case_url.split('/')[-1]
+    payload = urllib.request.urlopen(new_case_url).read()
 
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
+    # Predicting 
+    prediction = predict(payload)
+
+    if prediction ==  'Intracranial':
+      prediction_record = (file_name, 'True', 'False', 'False', 'Intracranial Hemorrhage', datetime.now())
+    elif prediction ==  'Mass Effect':
+      prediction_record = (file_name, 'False', 'True', 'False', 'Mass Effect', datetime.now())
+    elif prediction ==  'Midline Shift':
+      prediction_record = (file_name, 'False', 'False', 'True', 'Midline Shift', datetime.now())
+ 
+    # Inserting into database
+    insert_in_database(prediction_record)
+
+    print('Prediction successfully saved in Database!')
+  
+    return 'Success'
+  except Exception as ex:
+    return ex
+
+
+def predict(payload):
+   # Configurations
+   region = os.environ['REGION'] 
+   access_id = os.environ['ACCESS_ID']
+   secret_key = os.environ['SECRET_KEY']
+   endpoint = 'brain-model-ep--2020-06-16-14-46-14'
+   boto_session = boto3.Session(region_name=region, aws_access_key_id=access_id, aws_secret_access_key=secret_key)
+   session = sagemaker.Session(boto_session=boto_session)
+   predictor = RealTimePredictor(endpoint=endpoint, sagemaker_session=session, serializer=None)
+   class_mappings = ['Intracranial', 'Mass Effect', 'Midline Shift'] 
+
+   # Predicting
+   prediction = json.loads(predictor.predict(payload))
+   return  class_mappings[np.argmax(prediction)]
+
+
+def insert_in_database(prediction_record):
+    conn = None
+    try:
+        # Configurations
+        db_user = os.environ['DB_USER']
+        db_password = os.environ['DB_PASSWORD'] 
+        db_host = os.environ['DB_HOST'] 
+        db_port = os.environ['DB_PORT'] 
+
+        # Establishing Connection
+        conn = psycopg2.connect(user = db_user, password = db_password, host = db_host, port = db_port)
+        conn.autocommit = True
+
+        # Query Definitions
+        insert_query = 'INSERT INTO prediction (file_name, intracranial_hemorrhage, mass_effect, midline_shift, prediction, prediction_date) VALUES(%s,%s,%s,%s,%s,%s)'
+
+        # Record Insertion
+        cursor = conn.cursor()
+        cursor.execute(insert_query, prediction_record)
+        
+        return 'Success'
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn:
+            conn.close()
